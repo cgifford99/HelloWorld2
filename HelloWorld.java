164b3639ee541fa2c790b1c27c0a36d566c85eb2
cgifford99 <cmgskiguy99@gmail.com>
2021-02-03 10:01:50 -0500

    Said Hi to Alice

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("Here ye : important announcement");
         System.out.println("Hello World!");
+	System.out.println("Hey Alice!");
 	System.out.println("Goodbye!");
     }
 }
